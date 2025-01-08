6f0fccdfcc3dd3fe975459decb6a531f51b6eabf
Igor <igorlis159@gmail.com>
2025-01-08 19:55:17 +0200

    Project created

---
+++ src/test/java/com/example/ShortenerProject/ShortenerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShortenerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
