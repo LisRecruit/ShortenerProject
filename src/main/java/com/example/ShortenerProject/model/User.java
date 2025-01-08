f80ef431e4d1e7bf7519f9b4720c3484bf1d8a3b
gtsasiuk <gtsasuk@gmail.com>
2025-01-08 22:08:18 +0200

    Created migration and entities with repositories for DB

---
+++ src/main/java/com/example/ShortenerProject/model/User.java
@@ -0,0 +1,25 @@
+package com.example.ShortenerProject.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", unique = true, nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    List<ShortUrl> urls = new ArrayList<ShortUrl>();
+}
