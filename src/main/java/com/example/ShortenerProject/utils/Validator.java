9b58020be29ec2a308548b194cc231b5e5981613
gtsasiuk <gtsasuk@gmail.com>
2025-01-16 21:20:08 +0200

    Added validation for user password

--- src/main/java/com/example/ShortenerProject/utils/Validator.java
+++ src/main/java/com/example/ShortenerProject/utils/Validator.java
@@ -1,4 +1,16 @@
 package com.example.ShortenerProject.utils;
 
+import java.util.regex.Pattern;
+
 public class Validator {
+    private static final String PASSWORD_PATTERN =  "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$";
+
+    private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+
+    public static boolean isValidPassword(String password) {
+        if (password == null) {
+            return false;
+        }
+        return pattern.matcher(password).matches();
+    }
 }
