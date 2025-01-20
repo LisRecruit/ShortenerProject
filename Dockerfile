c6f9218ca32b137816183de8e4b8aa88104dccba
Martin Dumas <dumasmartin9@gmail.com>
2025-01-20 23:17:29 +0200

    configured docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM openjdk:21-slim
 MAINTAINER shortUrl.com
 COPY build/libs/ShortenerProject-0.0.1-SNAPSHOT.jar ShortenerProject.jar
+ENV SPRING_PROFILES_ACTIVE=prod
 ENTRYPOINT ["java", "-jar", "/ShortenerProject.jar"]
--- src/main/resources/db/migration/V1__Init_DB_tables.sql
+++ src/main/resources/db/migration/V1__Init_DB_tables.sql
@@ -1,16 +1,16 @@
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE short_urls (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     short_url VARCHAR(255) UNIQUE NOT NULL,
     origin_url VARCHAR(255) NOT NULL,
     date_of_creating DATE,
     date_of_expiring DATE,
-    count_of_transition LONG,
+    count_of_transition BIGINT,
     user_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
