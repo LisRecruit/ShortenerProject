db5a406c9f74058a531cc44407c5f34cd6fd0c1c
Igor <igorlis159@gmail.com>
2025-01-20 03:49:23 +0200

    Added JWT to swagger config (OpenApiConfig)

--- src/main/java/com/example/ShortenerProject/configuration/OpenApiConfig.java
+++ src/main/java/com/example/ShortenerProject/configuration/OpenApiConfig.java
@@ -1,7 +1,9 @@
 package com.example.ShortenerProject.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,7 +16,12 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Short URL API")
                         .version("1.0")
-                        .description("API for creating and managing shortened URLs"));
+                        .description("API for creating and managing shortened URLs"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
 
