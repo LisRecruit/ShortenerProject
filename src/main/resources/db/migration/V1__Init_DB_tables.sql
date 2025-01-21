589f1f7339647835e80fb638be769a924e7991bc
gtsasiuk <gtsasuk@gmail.com>
2025-01-21 14:20:01 +0200

    Changed DB migration to h2 dialect

--- src/main/resources/db/migration/V1__Init_DB_tables.sql
+++ src/main/resources/db/migration/V1__Init_DB_tables.sql
@@ -1,11 +1,11 @@
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE short_urls (
-    id SERIAL PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     short_url VARCHAR(255) UNIQUE NOT NULL,
     origin_url VARCHAR(255) NOT NULL,
     date_of_creating DATE,
